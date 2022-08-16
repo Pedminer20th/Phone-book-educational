1ba8ac6579989493cf6362f49e1ed4863688c3ab
Pedminer20th <pedram.baniardalan@gmail.com>
2022-08-17 00:37:54 +0430

    new edit for less numbers

--- main code.py
+++ main code.py
@@ -1,28 +1,28 @@
 # V_DEMO
 
 contacts = []
-print("""choose a number
-1:Add
-2:List
-3:Search
-4:Number edit
-0:Exit""")
+print("""choose an option :
+Add:Add a contact (name and mobile)
+List:show the current List
+Search:Search via name
+Edit:Number edit
+Exit:Exit the program""")
 choice = input("Enter your choice : ")
-while choice != '0':
-    if choice == '1':
+while choice != 'Exit':
+    if choice == 'Add':
         name = input("Enter the name : ")
         mobile = input("Enter the number : ")
         contact = [name, mobile]
         contacts.append(contact)
-    if choice == '2':
+    if choice == 'List':
         print("your current list is : ", contacts)
-    if choice == '3':
+    if choice == 'Search':
         search = input("Enter the name for search : ")
         for i in range(len(contacts)):
             if contacts[i][0] == search:
                 print("The result is : ",
                       contacts[i][0], " : ", contacts[i][1])
-    if choice == '4':
+    if choice == 'Edit':
         print("heres your list :", contacts)
         change = input("Enter the contact you want to edit : ")
         for i in range(len(contacts)):
@@ -30,10 +30,10 @@ while choice != '0':
                 print("Heres the old number : ", contacts[i][1])
                 new_number = input("Enter the new number : ")
                 contacts[i][1] = new_number
-    print("""choose a number
-    1:Add
-    2:List
-    3:Search
-    4:Number edit 
-    0:Exit""")
+    print("""choose an option :
+    Add:Add a contact (name and mobile)
+    List:show the current List
+    Search:Search via name
+    Edit:Number edit
+    Exit:Exit the program""")
     choice = input("Enter your choice : ")
--- readme.md
+++ readme.md
@@ -1,2 +1,6 @@
-hello
-this is a test project
+The phone book project
+
+made by Pedminer20th
+
+next step : 1- Delete(feature)
+2 -
