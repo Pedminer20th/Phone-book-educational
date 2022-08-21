0a23c85391ecc6544090e8ecf89f5f695df41ed4
Pedminer20th <pedram.baniardalan@gmail.com>
2022-08-21 13:26:47 +0430

    delete added

--- main code.py
+++ main code.py
@@ -6,6 +6,7 @@ Add:Add a contact (name and mobile)
 List:show the current List
 Search:Search via name
 Edit:Number edit
+Delete:Dlete a contact
 Exit:Exit the program""")
 choice = input("Enter your choice : ")
 while choice != 'Exit':
@@ -14,15 +15,15 @@ while choice != 'Exit':
         mobile = input("Enter the number : ")
         contact = [name, mobile]
         contacts.append(contact)
-    if choice == 'List':
+    elif choice == 'List':
         print("your current list is : ", contacts)
-    if choice == 'Search':
+    elif choice == 'Search':
         search = input("Enter the name for search : ")
         for i in range(len(contacts)):
             if contacts[i][0] == search:
                 print("The result is : ",
                       contacts[i][0], " : ", contacts[i][1])
-    if choice == 'Edit':
+    elif choice == 'Edit':
         print("heres your list :", contacts)
         change = input("Enter the contact you want to edit : ")
         for i in range(len(contacts)):
@@ -30,6 +31,14 @@ while choice != 'Exit':
                 print("Heres the old number : ", contacts[i][1])
                 new_number = input("Enter the new number : ")
                 contacts[i][1] = new_number
+    elif choice == 'Delete' :
+        print(contacts)
+        dlt = input("which one do you want to delete : ")
+        for i in range (len(contacts)-1 , -1 , -1) :
+            if contacts[i][0] == dlt or contacts[i][1] == dlt :
+                del contacts[i]         
+    else :
+        print("Choose wisely !!! ")
     print("""choose an option :
     Add:Add a contact (name and mobile)
     List:show the current List
